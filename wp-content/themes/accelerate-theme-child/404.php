879adbdcd3e41e3c52dae8bb951fef359acc6023
Veronica Armstrong <varmstrong27@gmail.com>
2016-05-17 12:10:02 -0700

    added 404 error page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -9,10 +9,12 @@
 
 get_header(); ?>
 
-<div class="error-page">
-    <h3>These aren't the droids you're looking for...</h3>
+<div class="error-image">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/404.jpg"/>
 </div>
 
-
+<div class="error-message">
+    <h3>These aren't the droids you're looking for...</h3>
+</div>
 
 <?php get_footer(); ?>
